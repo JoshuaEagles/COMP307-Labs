30a4c6e835480536a0c700d4a5199202b0fa1bea
JoshuaEagles <joshuaeagles1@gmail.com>
2022-09-18 14:40:12 -0400

    commit lab 2 files

---
+++ Week 2 Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Welcome to the n+1 program.\nJoshua Eagles (301078033) & Son Roy Almerol ()");
+
+while (true) {
+	Console.Write("Please enter an integer: ");
+
+	string inputStr = Console.ReadLine() ?? "";
+
+	bool isSuccess = int.TryParse(inputStr, out int parsedInput);
+
+	if (!isSuccess) 
+	{
+		Console.WriteLine($"{inputStr} is not a valid integer.");
+	}
+	else if (parsedInput >= int.MaxValue)
+	{
+		Console.WriteLine($"{parsedInput} is out of range.");
+	}
+	else 
+	{
+		Console.WriteLine($"{parsedInput} + 1 = {parsedInput + 1}");
+		break;
+	}
+}
